82768edc65d1b2eb35013b0260027fd0d42e62d3
Eimrine's Point of View <eimrine@users.noreply.github.com>
2014-11-19 05:59:15 +0200

    Update Magikan.py

--- Magikan.py
+++ Magikan.py
@@ -1,2 +1,3 @@
+""" http://geektimes.ru/post/111843/#comment_7880507 """
 for i in ("FizzBuzz" if i % 3 + i % 5 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else i for i in xrange(100)):
     print i
